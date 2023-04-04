502c1cfeadb2366ffb9b80357202f81554f5082b
Iman Mahmoudinasab <imanmahmoudinasab@gmail.com>
2023-04-04 08:14:43 -0700

    make subject postfix configurable

--- engine/subject-creator.js
+++ engine/subject-creator.js
@@ -1,12 +1,14 @@
 import fs from "fs-extra";
 import path from "path";
+import { getConfig } from "./config";
 
 export async function createSubjectTemplate(params) {
   const { templateDir, emailTemplateName, subject } = params;
 
+  const subjectPostfix = getConfig().subjectPostfix || "_subject";
   const subjectTemplatePath = path.join(
     templateDir,
-    emailTemplateName + "_Subject.html" // todo: make it configurable
+    emailTemplateName + subjectPostfix + ".html"
   );
 
   fs.ensureDirSync(path.dirname(subjectTemplatePath));
