4eed174e7882b871454270829c736712842d0de2
Iman Mahmoudinasab <imanmahmoudinasab@gmail.com>
2023-08-19 19:03:52 -0700

    [api] add js file extension to exports

--- engine/api.js
+++ engine/api.js
@@ -1,3 +1,3 @@
-export { compile } from "./compiler";
-export { getConfig } from "./config";
-export { getEmailTemplatesList } from "./template-finder";
+export { compile } from "./compiler.js";
+export { getConfig } from "./config.js";
+export { getEmailTemplatesList } from "./template-finder.js";
