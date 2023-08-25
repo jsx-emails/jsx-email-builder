ba0e8b8935a4d107b79ecfb5eabaca9ee9deb62e
Iman Mahmoudinasab/Cloud Solution /SRCA/Engineer/Samsung Electronics <iman.m@samsung.com>
2023-08-25 11:42:21 -0700

    [compiler] fix localized object in result object is not initialized

--- engine/compiler.js
+++ engine/compiler.js
@@ -33,6 +33,7 @@ export async function compile(options) {
 
   /** @type {CompileResult} */
   const result = {};
+  result.localized = {};
   let entryFileName;
   let bundleFileName;
   let i18next;
